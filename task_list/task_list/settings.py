867c2ca61f6373d5871e65e97731f3cadee33360
scad89 <71154471+scad89@users.noreply.github.com>
2022-01-29 05:16:29 +0300

    rewrite gitignore, corrected docker_compose file, optimized settings

--- task_list/task_list/settings.py
+++ task_list/task_list/settings.py
@@ -12,6 +12,7 @@ from datetime import timedelta
 import os
 from pathlib import Path
 
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
